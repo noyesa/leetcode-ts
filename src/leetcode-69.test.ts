e3b2e3aee38d137c6acd344f137774c7acae44b8
Andrew Noyes <noyesa@gmail.com>
2025-01-02 17:33:13 -0800

    Solved leetcode #69
    Implemented mySqrt function using binary search.

--- src/leetcode-69.test.ts
+++ src/leetcode-69.test.ts
@@ -1,7 +1,29 @@
 import { describe, it } from 'node:test';
 import assert from 'node:assert';
 
-function mySqrt(x: number): number {}
+/**
+ * Find the nearest integer square root of a number. Returns the floor of the
+ * actual square root.
+ * @param x - The number to find the square root of.
+ * @returns The square root of X rounded down to the nearest integer
+ */
+function mySqrt(x: number): number {
+  let bestGuess = NaN;
+  let left = 0;
+  let right = Math.ceil(x / 2);
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (mid * mid <= x) {
+      // Each successive guess that passes the predicate is larger than the
+      // last and is therefore closer to the correct answer.
+      bestGuess = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return bestGuess;
+}
 
 describe('mySqrt', () => {
   it('solves example #1', () => {
@@ -10,5 +32,9 @@ describe('mySqrt', () => {
 
   it('solves example #2', () => {
     assert.equal(mySqrt(8), 2);
+  });
+
+  it('handles 0', () => {
+    assert.equal(mySqrt(0), 0);
   });
 });
