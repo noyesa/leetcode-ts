2c1ab5ad4a1d60341a8514e4b4e60789d0775f37
Andrew Noyes <noyesa@gmail.com>
2025-01-02 22:24:38 -0800

    Added ListNode.isEqual for comparing linked lists
    Added isEqual method to compare two linked lists to determine whether
    they are equal. Two lists are equal when each successive node in either
    list has the same value and both lists have the same number of nodes.

--- src/leetcode-2.test.ts
+++ src/leetcode-2.test.ts
@@ -19,6 +19,30 @@ class ListNode {
   }
 
   /**
+   * Compares the values of two linked lists in order to determine whether they
+   * contain the same values.
+   * @param other - The other list to compare this with.
+   * @returns Do the lists contain equal values?
+   */
+  isEqual(other: ListNode): boolean {
+    if (this === other) {
+      return true;
+    }
+
+    let thisHead: ListNode | null = this;
+    let otherHead: ListNode | null = other;
+    while (thisHead && otherHead) {
+      if (thisHead.val !== otherHead.val) {
+        return false;
+      }
+      thisHead = thisHead.next;
+      otherHead = otherHead.next;
+    }
+
+    return !(thisHead || otherHead);
+  }
+
+  /**
    * Builds a linked list of numbers from an array of numbers.
    * @param array - Array to convert to linked list.
    * @returns Linked list created from values in array.
@@ -48,6 +72,25 @@ describe('ListNode', () => {
     assert.equal(node.next, dummy);
   });
 
+  describe('isEqual', () => {
+    it('returns false if an unequal list is passed', () => {
+      const list1 = ListNode.fromArray([1, 2, 3]) as ListNode;
+      const list2 = ListNode.fromArray([2, 3, 4]) as ListNode;
+      assert.equal(list1.isEqual(list2), false);
+    });
+
+    it('returns true if the same list is passed', () => {
+      const list1 = ListNode.fromArray([1, 2, 3]) as ListNode;
+      assert.ok(list1.isEqual(list1));
+    });
+
+    it('returns true if an equal list is passed', () => {
+      const list1 = ListNode.fromArray([1, 2, 3]) as ListNode;
+      const list2 = ListNode.fromArray([1, 2, 3]) as ListNode;
+      assert.ok(list1.isEqual(list2));
+    });
+  });
+
   describe('fromArray', () => {
     it('builds a linked list from an array', () => {
       const list = ListNode.fromArray([4, 1, 2]);
@@ -70,3 +113,5 @@ describe('ListNode', () => {
     });
   });
 });
+
+describe('addTwoNumbers', () => {});
