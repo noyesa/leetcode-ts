2c8e7c6088421a7c418b903c9ab910aaa9a7ed93
Andrew Noyes <noyesa@gmail.com>
2025-02-17 02:46:09 -0800

    More fun

--- src/leetcode-186.test.ts
+++ src/leetcode-186.test.ts
@@ -23,6 +23,48 @@ describe('reverseSubarray', () => {
 });
 
 /**
+ * Finds the next index beginning with start that passes the predicate.
+ * @param a - The list within which to search.
+ * @param start - The start index from which to begin search.
+ * @param pred - Callback that will be invoked with each value.
+ * @returns The index of the next passing value after start.
+ */
+function findNextIndex<T>(a: T[], start: number, pred: (arg0: T) => boolean) {
+  for (let i = start; i < a.length; ++i) {
+    if (pred(a[i])) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+describe('findNextIndex', () => {
+  it('finds the first instance by default', () => {
+    const nums = [1, 2, 3, 4, 3];
+    assert.equal(
+      findNextIndex(nums, 0, (x) => x === 2),
+      1,
+    );
+  });
+
+  it('finds instances after the start index', () => {
+    const nums = [1, 2, 3, 2, 5];
+    assert.equal(
+      findNextIndex(nums, 2, (x) => x === 2),
+      3,
+    );
+  });
+
+  it('returns -1 when no value passes the predicate', () => {
+    const nums = [1, 2, 3, 4, 5];
+    assert.equal(
+      findNextIndex(nums, 0, (x) => x === 10),
+      -1,
+    );
+  });
+});
+
+/**
  * Takes a character array s and reverses the order of the words. This must be
  * done in place, without allocating any additional memory.
  * @param s - The string represented as an array of characters.
@@ -31,25 +73,20 @@ function reverseWords(s: string[]): void {
   // Reverse the entire array.
   reverseSubarray(s, 0, s.length - 1);
 
-  // Swap all the words in the array.
+  // Find the beginning of a word.
   let i = 0;
-  while (i < s.length) {
-    // Find the beginning of a word.
-    if (s[i] === ' ') {
-      ++i;
-    } else {
-      // Find the end of the word.
-      let j = s.indexOf(' ', i + 1);
-      if (j === -1) {
-        j = s.length;
-      }
-
-      // Reverse the word.
-      reverseSubarray(s, i, j - 1);
-
-      // Advance to the next word, skipping the space.
-      i = j + 1;
+  while ((i = findNextIndex(s, i, (c) => c !== ' ')) !== -1) {
+    // Find the end of the word.
+    let j = s.indexOf(' ', i + 1);
+    if (j === -1) {
+      j = s.length;
     }
+
+    // Reverse the word.
+    reverseSubarray(s, i, j - 1);
+
+    // Advance to the next word, skipping the space.
+    i = j + 1;
   }
 }
 
